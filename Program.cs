9fff09585078ea8f5c9895b7a6b7f37e1a6ae298
Alessandro Tulioli <alessandro.tulioli@csu.com.br>
2024-01-26 18:07:39 -0300

    Inicio do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Servidor_HTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servidorHttp = new ServidorHttp();
+        }
+    }
+}
